c1434d68836b951d0e545d3ffa6d2d63ee0d1455
Jamie Jones <jbjonesjr@github.com>
2021-07-08 16:00:09 +0000

    start building out weather api

--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,20 @@ app.get('/api/test', (req, res) => {
   console.log(`responded to /api/test route`);
 });
 
+let fetch_current_conditions = ()=>{
+    return conditions = {
+        temperate_f: 00,
+        humidty_perc: 00,
+        wind_dir: 00,
+        wind_speed_mph: 00,
+        rainfall_rate_in: 00
+    };
+}
+app.get('/api/weather/current', (req, res) => {
+  let conditions = fetch_current_conditions()
+    res.json(conditions);
+});
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {

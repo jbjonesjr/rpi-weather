e901df1b5e17ea82a04cc572bf65b76e0548d395
Jamie Jones <jbjonesjr@github.com>
2021-07-09 03:11:24 +0000

    work on rtl code

---
+++ src/rtl-publish/index.js
@@ -0,0 +1,106 @@
+const { Client } = require('pg');
+//docs: https://www.npmjs.com/package/postgres
+
+/*
+
+inspired by https://github.com/Ax-LED/pimatic-rtl433/blob/master/rtl433.coffee
+for reference: https://github.com/merbanan/rtl_433
+  spawn = require('child_process').spawn
+      proc = spawn("#{__dirname}/bin/rtl_433",['-f', @config.freq, '-R', '03', '-R', '19', '-R', '52', '-R', '141', '-F', 'csv', '-l', @config.detectionLevel])
+
+       proc.stdout.setEncoding('utf8')
+      proc.stderr.setEncoding('utf8')
+      rl = readline.createInterface({ input: proc.stdout })
+
+      rl.on('line', (line) => 
+        @_dataReceived(line)
+      )
+
+      proc.stderr.on('data',(data) =>
+        lines = data.split(/(\r?\n)/g)
+        env.logger.warn line for line in lines when line.trim() isnt ''
+      )
+
+      proc.on('close',(code) =>
+        if code!=0
+          env.logger.error "rtl_433 returned", code
+        rl.close()
+      )
+
+    _dataReceived: (data) ->
+      env.logger.debug data
+      datas = {};
+      datas = data.split(",")
+      if datas.length == 15 #Data Length variiert, abhängig von den RTL_433 Parametern
+        result = {}
+        result = {
+            "model": datas[3],
+            "sensorId": datas[5],
+            "channel": datas[7],
+            "lowbattery": datas[8],
+            "temperatureC": parseFloat(datas[9]),
+            "humidity": parseInt(datas[10]),
+            "temperatureF": parseFloat(datas[12])
+        }
+        env.logger.debug "Got measure (model:" + result.model + ", sensorId: " + result.sensorId + ", channel: " + result.channel + ", lowbattery:" + result.lowbattery + ", TempC:" + result.temperatureC + ", Humidity:" + result.humidity + ", TempF:" + result.temperatureF + ")"
+        @emit('temp', result)
+
+
+*/
+
+// use process.env.DATABASE_URL instead
+const client = new Client({
+  connectionString: "postgres://lovtaqracvoeyl:c11f64483882db502aa521cea87e2383af294b06e68ca161eef66aa7850f2db1@ec2-54-152-185-191.compute-1.amazonaws.com:5432/d3fit8gdg1cd51",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
+/*
+const users = [{
+  name: 'Murray',
+  age: 68,
+  garbage: 'ignore'
+}, {
+  name: 'Walter',
+  age: 78
+}]
+ 
+sql`
+  insert into users ${
+    sql(users, 'name', 'age')
+  }
+`
+
+const [user, account] = await sql.begin(async sql => {
+  const [user] = await sql`
+    insert into users (
+      name
+    ) values (
+      'Alice'
+    )
+  `
+ 
+  const [account] = await sql`
+    insert into accounts (
+      user_id
+    ) values (
+      ${ user.user_id }
+    )
+  `
+ 
+  return [user, account]
+})
+
+*/
+const [new_user] = await client`
+  insert into reports (
+    
+  ) values (
+    'Murray', 68
+  )
+ 
+  returning *
+`
+client.qu
---
+++ src/rtl-publish/schema.sql
@@ -0,0 +1,26 @@
+CREATE TABLE reports
+(
+    pid serial NOT NULL,
+    -- autoincrement
+    created_on timestamp without time zone NOT NULL,
+    observed_at timestamp without time zone NOT NULL,
+    sensor_id character integer NOT NULL,
+    temperature_f double precision NULL,
+    humidity double precision NULL,
+    wind_kph double precision NULL,
+    wind_dir character NULL,
+    wind_dir_deg double precision NULL,
+    rain_first integer NULL,
+    rain_second integer NULL,
+    rain_rate double precision NULL,
+    pressure_mb double precision NULL,
+    pressure_in double precision NULL
+);
+
+CREATE TABLE sensors
+(
+    pid serial NOT NULL,
+    created_on timestamp without time zone NOT NULL,
+    sensor_id character varying(7) NOT NULL,
+    sensor_type character varying(20) NOT NULL
+);

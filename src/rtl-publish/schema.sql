33d8236b31f7ba06d42c9651fb54e22f41c8c68b
Jamie Jones <jbjonesjr@github.com>
2022-06-11 08:22:30 +0000

    improve schema

--- src/rtl-publish/schema.sql
+++ src/rtl-publish/schema.sql
@@ -1,27 +1,35 @@
+DROP TABLE reports;
+
 CREATE TABLE reports
 (
     pid serial NOT NULL,
     -- autoincrement
     created_on timestamp without time zone NOT NULL,
     observed_at timestamp without time zone NOT NULL,
-    sensor_id character integer NOT NULL,
+    seq integer NOT NULL,
+    sensor_id integer NOT NULL,
+    lowbattery integer NULL,
+    battery_ok integer NULL,
+    mic character varying(7) NULL,
+    startup integer NULL,
     temperature_f double precision NULL,
     humidity double precision NULL,
     wind_kph double precision NULL,
-    wind_dir character NULL,
     wind_dir_deg double precision NULL,
-    rain_first integer NULL,
-    rain_second integer NULL,
-    rain_diff double precision NULL,
+    rain_first_mm double precision NULL,
+    rain_second_mm double precision NULL,
+    rain_diff_mm double precision NULL,
     rain_rate double precision NULL,
-    pressure_mb double precision NULL,
-    pressure_in double precision NULL
+    pressure_mb double precision NULL
 );
+
+DROP TABLE sensors;
 
 CREATE TABLE sensors
 (
-    pid serial NOT NULL,
+    pid serial,
     created_on timestamp without time zone NOT NULL,
     sensor_id character varying(7) NOT NULL,
-    sensor_type character varying(20) NOT NULL
+    sensor_type character varying(20) NOT NULL,
+    data_validity integer NOT NULL
 );

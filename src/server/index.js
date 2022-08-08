20c5111617652e1f2a66c456a14f3182564de953
Jamie Jones <jbjonesjr@github.com>
2022-08-08 04:20:39 +0000

    current conditions works

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const path = require('path');
+const { Client } = require('pg');
 //let rtl_process = require("../../src/rtl-publish/rtl_process.js");
 
 
-// use process.env.DATABASE_URL instead
-// const client = new Client({
-//   connectionString: "postgresql-defined-56618",
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+console.log("Connecting to database...", process.env.DATABASE_URL);
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+client.connect();
 
 const app = express();
 
@@ -33,33 +31,46 @@ app.get('/api/test', (req, res) => {
 });
 
 app.get('/api/weather/current', (req, res) => {
-  let conditions = fetch_current_conditions()
-    res.json(conditions);
+  
+  let conditions = fetch_current_conditions().then(result => {console.log(result); res.json(result);}).catch(err => {console.log(err); res.json(err);});
+  console.log('f(x) return', conditions);
+  
+  
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/../app/build/index.html'));
+  res.sendFile(path.join(__dirname + '/../app/build/index.html'));
 });
 
-let fetch_current_conditions = ()=>{
-  client.connect();
+let fetch_current_conditions = () => {
+  try {
+    console.log(`fetching current conditions`);
 
-  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
-    return conditions = {
-        temperate_f: 00,
-        humidty_perc: 00,
-        wind_dir: 00,
-        wind_speed_mph: 00,
-        rainfall_rate_in: 00
-    };
+    // Query the database for the current conditions and return them
+    return client.query(`SELECT observed_at as "time", TRUNC(temperature_f::numeric,2) as "temp_f", wind_kph as "wind", wind_dir_deg as wind_dir, rain_diff_mm as "rain", humidity FROM reports WHERE  NOT EXISTS (SELECT * FROM reports AS reports_temp WHERE reports.sensor_id = reports_temp.sensor_id AND reports.observed_at < reports_temp.observed_at) and sensor_id = '2';`)
+      .then(result => {
+        console.log(`fetched current conditions`);
+
+        return conditions = {
+          obs: result.rows[0].oberved_at,
+          temperate_f: result.rows[0].temp_f,
+          humidty_perc:  result.rows[0].humidity,
+          wind_dir: result.rows[0].wind_dir,
+          wind_speed_mph: result.rows[0].wind_kph,
+          rainfall_rate_in: result.rows[0].rain_rate
+        };
+        // return result.rows[0];
+      }).catch(err => {
+        console.log(`error fetching current conditions`);
+        console.log(err);
+      });
+  }
+  catch (err) {
+    console.log(`error fetching current conditions`);
+    console.log(err);
+  }
 }
 
 const port = process.env.PORT || 5000;

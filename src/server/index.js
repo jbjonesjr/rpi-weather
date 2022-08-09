f0270ec1668c1f00b2cac3bfbac4f94b9e5ab0b9
Jamie Jones <jbjonesjr@github.com>
2022-08-09 02:17:36 +0000

    adding amanac code

--- src/server/index.js
+++ src/server/index.js
@@ -29,7 +29,7 @@ app.get('/api/test', (req, res) => {
 });
 
 app.get('/api/weather/current', (req, res) => {
-  let conditions = fetch_current_conditions().then(result => {console.log(result); res.json(result);}).catch(err => {console.log(err); res.json(err);});
+  let conditions = fetch_current_conditions().then(result => { console.log(result); res.json(result); }).catch(err => { console.log(err); res.json(err); });
   console.log('f(x) return', conditions);
 });
 
@@ -44,14 +44,29 @@ let fetch_current_conditions = () => {
     console.log(`fetching current conditions`);
 
     // Query the database for the current conditions and return them
-    return client.query(`SELECT observed_at as "time", TRUNC(temperature_f::numeric,2) as "temp_f", wind_kph as "wind", wind_dir_deg as wind_dir, rain_diff_mm as "rain", humidity FROM reports WHERE  NOT EXISTS (SELECT * FROM reports AS reports_temp WHERE reports.sensor_id = reports_temp.sensor_id AND reports.observed_at < reports_temp.observed_at) and sensor_id = '2';`)
+    const current_conditions_query = <sql>
+      SELECT observed_at as "time",
+      TRUNC(temperature_f::numeric,2) as "temp_f",
+      wind_kph as "wind", wind_dir_deg as wind_dir,
+      rain_diff_mm as "rain",
+      humidity
+      FROM reports
+      WHERE NOT EXISTS (
+      SELECT *
+      FROM reports AS reports_temp
+      WHERE reports.sensor_id = reports_temp.sensor_id
+      AND reports.observed_at < reports_temp.observed_at)
+      and sensor_id = '2';
+    </sql>
+
+    return client.query(current_conditions_query)
       .then(result => {
         console.log(`fetched current conditions`);
 
         return conditions = {
           obs: result.rows[0].oberved_at,
           temperate_f: result.rows[0].temp_f,
-          humidty_perc:  result.rows[0].humidity,
+          humidty_perc: result.rows[0].humidity,
           wind_dir: result.rows[0].wind_dir,
           wind_speed_mph: result.rows[0].wind_kph,
           rainfall_rate_in: result.rows[0].rain_rate

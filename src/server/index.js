3a15d229efb739133b665b538a21834f589fb406
Jamie Jones <jbjonesjr@github.com>
2022-08-09 03:05:56 +0000

    clean up sql queries and formatting

--- src/server/index.js
+++ src/server/index.js
@@ -14,6 +14,20 @@ client.connect();
 
 const app = express();
 
+const current_conditions_query = `
+SELECT observed_at as "time",
+TRUNC(temperature_f::numeric,2) as "temp_f",
+wind_kph as "wind", wind_dir_deg as wind_dir,
+rain_diff_mm as "rain",
+humidity
+FROM reports
+WHERE NOT EXISTS (
+SELECT *
+FROM reports AS reports_temp
+WHERE reports.sensor_id = reports_temp.sensor_id
+AND reports.observed_at < reports_temp.observed_at)
+and sensor_id = '2';
+`;
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*
@@ -29,8 +43,15 @@ app.get('/api/test', (req, res) => {
 });
 
 app.get('/api/weather/current', (req, res) => {
-  let conditions = fetch_current_conditions().then(result => { console.log(result); res.json(result); }).catch(err => { console.log(err); res.json(err); });
-  console.log('f(x) return', conditions);
+  let conditions = fetch_current_conditions()
+    .then(result => {
+      console.log('f(x) current conditions', result);
+      res.json(result);
+    })
+    .catch(err => {
+      console.log(err); res.json(err);
+    });
+});
 });
 
 // The "catchall" handler: for any request that doesn't
@@ -64,14 +85,24 @@ let fetch_current_conditions = () => {
         console.log(`fetched current conditions`);
 
         return conditions = {
-          obs: result.rows[0].oberved_at,
+          obs: result.rows[0].observed_at,
           temperate_f: result.rows[0].temp_f,
           humidty_perc: result.rows[0].humidity,
           wind_dir: result.rows[0].wind_dir,
           wind_speed_mph: result.rows[0].wind_kph,
           rainfall_rate_in: result.rows[0].rain_rate
         };
-        // return result.rows[0];
+      }).catch(err => {
+        console.log(`error fetching current conditions`);
+        console.log(err);
+      });
+  }
+  catch (err) {
+    console.log(`error fetching current conditions`);
+    console.log(err);
+  }
+}
+
       }).catch(err => {
         console.log(`error fetching current conditions`);
         console.log(err);

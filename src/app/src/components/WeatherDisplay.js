f866a7d5313f53b1f0a1c61f6800834c30eb37f0
Jamie Jones <jbjonesjr@github.com>
2022-08-20 16:57:05 +0000

    better almanac presentation

--- src/app/src/components/WeatherDisplay.js
+++ src/app/src/components/WeatherDisplay.js
@@ -6,7 +6,7 @@ const WeatherDisplay = ({ weather: { currentTemp, weatherMain, tempMax, tempMin,
   <StyledWeatherDisplay>
     <h1>{Math.round(currentTemp)}<span>&#186;F</span> </h1>
     <h2>{weatherMain}</h2>
-    <h3>Daily Alamanac (Min/Max): {Math.round(tempMin)}&#186;F / {Math.round(tempMax)}&#186;F {(totalRainfall+.00).toFixed(2)} in of rain</h3>
+    <h3>Daily Alamanac (Min/Max): {Math.round(tempMin)}&#186;F / {Math.round(tempMax)}&#186;F, {(totalRainfall+.00).toFixed(2)}" of rain</h3>
   </StyledWeatherDisplay>
 );
 

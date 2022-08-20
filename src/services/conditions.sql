b34c5b8046d24d9c3b0244921543e8dab0bcf8d4
Jamie Jones <jbjonesjr@github.com>
2022-08-20 17:02:32 +0000

    improved rain handling with no hourly report

--- src/services/conditions.sql
+++ src/services/conditions.sql
@@ -2,11 +2,11 @@
 SELECT (observed_at at time zone 'America/New_York') as "time", 
 TRUNC(temperature_f::numeric,2) as "temp_f", 
 wind_kph as "wind", wind_dir_deg as wind_dir, 
-current_rain.obsered_rainfall as "hourly_rain",
+COALESCE(current_rain.obsered_rainfall,0) as "hourly_rain",
 humidity 
 FROM reports 
-join ( 
-select sum(rain_mm) as "obsered_rainfall" from
+left join ( 
+select "time", sum(rain_mm) as "obsered_rainfall" from
 ( select date_trunc('hour', observed_at) as "time", rain_diff_mm as "rain_mm" from reports where sensor_id = 1 and date_trunc('hour', observed_at) = date_trunc('hour', now() at time zone 'America/New_York' at time zone 'utc' ) ) as "hourly rainfall"
 group by time order by time desc
 ) as "current_rain" on 1=1

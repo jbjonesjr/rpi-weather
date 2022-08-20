9a00ca9004e2c6d8f9bcee41109d9f217ccf1e2f
Jamie Jones <jbjonesjr@github.com>
2022-08-20 12:23:12 +0000

    Better styling, and rainfall in the display

--- src/app/src/components/WeatherDisplay.js
+++ src/app/src/components/WeatherDisplay.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 import StyledWeatherDisplay from './styles/StyledWeatherDisplay';
 
-const WeatherDisplay = ({ weather: { currentTemp, weatherMain, tempMax, tempMin } }) => (
+const WeatherDisplay = ({ weather: { currentTemp, weatherMain, tempMax, tempMin, totalRainfall } }) => (
   <StyledWeatherDisplay>
-    <h1>{Math.round(currentTemp)}<span>&#186;C</span> </h1>
+    <h1>{Math.round(currentTemp)}<span>&#186;F</span> </h1>
     <h2>{weatherMain}</h2>
-    <h3>{Math.round(tempMin)}&#186;C / {Math.round(tempMax)}&#186;C</h3>
-    {/* <h1>15<span>&#186;C</span> </h1>
-    <h2>Sunny</h2>
-    <h3>13&#186;C / 16&#186;C</h3> */}
+    <h3>Daily Alamanac (Min/Max): {Math.round(tempMin)}&#186;F / {Math.round(tempMax)}&#186;F {(totalRainfall+.00).toFixed(2)} in of rain</h3>
   </StyledWeatherDisplay>
 );
 

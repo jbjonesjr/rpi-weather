42331488848557daa35a5b80ccaa5e24e06e218b
Jamie Jones <jbjonesjr@github.com>
2022-08-20 01:57:45 +0000

    cors fix

--- src/server/index.js
+++ src/server/index.js
@@ -9,6 +9,12 @@ const __dirname = dirname(__filename);
 
 const app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+ 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use("/",serverRouter);
 

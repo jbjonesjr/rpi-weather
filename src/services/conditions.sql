cd76621029b87c07520a7fa535ad7588ec06b172
Jamie Jones <jbjonesjr@github.com>
2022-12-28 23:03:59 +0000

    add windchill equation to the conditions query

--- src/services/conditions.sql
+++ src/services/conditions.sql
@@ -3,7 +3,8 @@ SELECT (observed_at at time zone 'America/New_York') as "time",
 TRUNC(temperature_f::numeric,2) as "temp_f", 
 wind_kph as "wind", wind_dir_deg as wind_dir, 
 COALESCE(current_rain.obsered_rainfall,0) as "hourly_rain",
-humidity 
+humidity,
+(0.0817*(3.71* ((COALESCE(wind_kph,0.25))^0.5) + 5.81 - (0.25* ((COALESCE(wind_kph,0.25))^0.5)))*((TRUNC(temperature_f::numeric,2)) - 91.4) + 91.4) as "wind_chill_new"
 FROM reports 
 left join ( 
 select "time", TRUNC((sum(rain_mm)/25.4)::numeric,2) as "obsered_rainfall" from
@@ -16,3 +17,8 @@ WHERE NOT EXISTS (
   WHERE reports.sensor_id = reports_temp.sensor_id 
   AND reports.observed_at < reports_temp.observed_at) 
 and sensor_id = '2'
+
+# sql windchill equation
+# http://weather.uky.edu/aen599/wchart.htm
+
+#select temp, wind_mph, (0.0817*(3.71* (wind_mph^0.5) + 5.81 - (0.25* wind_mph))*(temp - 91.4) + 91.4) as "wind_chill" from (select 34 as temp, 3 as wind_mph) as "test"

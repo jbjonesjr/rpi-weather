46d115ae35784ac337027a9808e0b7182f768cf6
Jamie Jones <jbjonesjr@github.com>
2023-06-25 18:05:16 +0000

    bad copy pasta

--- src/app/src/components/Temp-chart.js
+++ src/app/src/components/Temp-chart.js
@@ -20,18 +20,6 @@ const data = {
       data: [[5,10], [10,20], [19,26], [26,27], [27,29], [27,28], [25,27], [20,25]],
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       borderColor: 'rgba(255, 99, 132, 1)',
-import { Bar } from 'react-chartjs-2';
-
-
-
-const data = {
-  labels: ["Midnight", "3 AM", "6 AM", "9 AM", "Noon", "3 PM", "6 PM", "9 PM"],
-  datasets: [
-    {
-      label: 'My Dataset',
-      data: [[5,10], [10,20], [19,26], [26,27], [27,29], [27,28], [25,27], [20,25]],
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
       borderWidth: 1,
     },
   ],

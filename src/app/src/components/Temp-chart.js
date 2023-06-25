43d56f11619c911beb91df620ed552e1ade3d31b
Jamie Jones <jbjonesjr@github.com>
2023-06-25 17:55:09 +0000

    more inports

--- src/app/src/components/Temp-chart.js
+++ src/app/src/components/Temp-chart.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from "chart.js";
 import { Bar } from 'react-chartjs-2';
 
 

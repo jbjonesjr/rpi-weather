45ec7ed8d15c4538d0995558789b72a761d45b0c
Jamie Jones <jbjonesjr@github.com>
2023-06-25 18:02:18 +0000

    more chart setup

--- src/app/src/components/Temp-chart.js
+++ src/app/src/components/Temp-chart.js
@@ -10,6 +10,18 @@ import {
 } from "chart.js";
 import { Bar } from 'react-chartjs-2';
 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, registerables);
+
+const data = {
+  labels: ["Midnight", "3 AM", "6 AM", "9 AM", "Noon", "3 PM", "6 PM", "9 PM"],
+  datasets: [
+    {
+      label: 'My Dataset',
+      data: [[5,10], [10,20], [19,26], [26,27], [27,29], [27,28], [25,27], [20,25]],
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+import { Bar } from 'react-chartjs-2';
+
 
 
 const data = {

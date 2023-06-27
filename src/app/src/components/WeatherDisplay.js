7cab94353ca22aec602abf3d9deeae990e44cca3
Jamie Jones <jbjonesjr@github.com>
2023-06-27 05:47:30 +0000

    protect against rainfall bug

--- src/app/src/components/WeatherDisplay.js
+++ src/app/src/components/WeatherDisplay.js
@@ -3,10 +3,11 @@ import React from 'react';
 import StyledWeatherDisplay from './styles/StyledWeatherDisplay';
 
 const WeatherDisplay = ({ weather: { currentTemp, weatherMain, tempMax, tempMin, totalRainfall } }) => (
-  <StyledWeatherDisplay>
+
+ <StyledWeatherDisplay>
     <h1>{Math.round(currentTemp)}<span>&#186;F</span> </h1>
     <h2>{weatherMain}</h2>
-    <h3>Daily Alamanac (Min/Max): {Math.round(tempMin)}&#186;F / {Math.round(tempMax)}&#186;F, {(totalRainfall+.00).toFixed(2)}" of rain</h3>
+    <h3>Daily Alamanac (Min/Max): {Math.round(tempMin)}&#186;F / {Math.round(tempMax)}&#186;F, {Number(totalRainfall+.00).toFixed(2)}" of rain</h3>
   </StyledWeatherDisplay>
 );
 
